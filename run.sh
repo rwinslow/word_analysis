2f9d868b789bcfb2e0eb40c16b65496ec299840a
Rich Winslow <rwins4@gmail.com>
2015-03-18 19:38:25 -0700

    Added install of python3 to shell script

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+# Load dependencies
+apt-get install python3
+
+# Set permissions for program
 chmod a+x ./src/word_analysis.py
 
+# Run program with the input directory wc_input and output files wc_result and med_result in wc_output
 python3 ./src/word_analysis.py ./wc_input ./wc_output/wc_result.txt ./wc_output/med_result.txt
